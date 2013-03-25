ffb22ce33c78b9ac77f9f2e6b497c113a438f370
vasyl_m <vasyl_m@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-25 02:42:53 +0000

    Version 1.7.2
    git-svn-id: https://plugins.svn.wordpress.org/advanced-access-manager/trunk@686787 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mvb_wp_access.php
+++ mvb_wp_access.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: Advanced Access Manager
   Description: Manage Access to WordPress Backend and Frontend.
-  Version: 1.7.1
+  Version: 1.7.2
   Author: Vasyl Martyniuk <martyniuk.vasyl@gmail.com>
   Author URI: http://www.whimba.org
  */
